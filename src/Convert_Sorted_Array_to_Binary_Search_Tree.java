15bbccae385a3e02e306e1374e4e38eaea1f7be5
Xinli Wang <wxlmaple@hotmail.com>
2015-11-19 16:36:18 -0500

    convert sorted array to BST

--- src/Convert_Sorted_Array_to_Binary_Search_Tree.java
+++ src/Convert_Sorted_Array_to_Binary_Search_Tree.java
@@ -20,14 +20,13 @@ public class Convert_Sorted_Array_to_Binary_Search_Tree {
         }
         return helper(nums,0,nums.length-1);
     }
-    //we make the middle of the array to be the root every time
+    //each time we make the middle of the array to be the root
     private TreeNode helper(int[] nums, int begin, int end){
         if(begin>end) return null;
         int middle = begin + (end - begin)/2;
-        TreeNode n = new TreeNode(nums[middle]);
-        n.left = helper(nums, begin , middle-1);
-        n.right = helper(nums, middle+1, end);
-        return n;
-
+        TreeNode node = new TreeNode(nums[middle]);
+        node.left = helper(nums, begin , middle-1);
+        node.right = helper(nums, middle+1, end);
+        return node;
     }
 }
