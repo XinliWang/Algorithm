e7b7276b88ba0369e7ab67b197cfc55117dff503
Xinli Wang <wxlmaple@hotmail.com>
2015-11-21 00:06:34 -0500

    Inorder Successor in BST

---
+++ src/Inorder_Successor_in_BST.java
@@ -0,0 +1,49 @@
+/**
+ * Given a binary search tree and a node in it,
+ * find the in-order successor(the next node of this specific node) of that node in the BST.
+
+ Note: If the given node has no in-order successor in the tree, return null.
+ */
+
+
+import java.util.ArrayList;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Inorder_Successor_in_BST {
+    /**
+     * Solution1
+     */
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        ArrayList<TreeNode> list = new ArrayList<>();
+        helper(list,root);
+        int index = -1;
+        for(index=0;index<list.size();index++){
+            if(list.get(index).val==p.val){
+                break;
+            }
+        }
+        if(index == list.size()-1)return null;
+        return list.get(index+1);
+    }
+
+    private void helper(ArrayList<TreeNode> list,TreeNode node){
+        if (node == null)return;
+        helper(list,node.left);
+        list.add(node);
+        helper(list,node.right);
+    }
+
+
+    /**
+     * Solution2:
+     */
+    
+}
