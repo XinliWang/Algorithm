ca66f2d3104e7227c69f1981511dbfdd5f1be1d9
Xinli Wang <wxlmaple@hotmail.com>
2015-11-30 23:24:54 -0500

    simply the same tree solution

--- src/Same_Tree.java
+++ src/Same_Tree.java
@@ -5,10 +5,8 @@
  */
 public class Same_Tree {
     public boolean isSameTree(TreeNode p,TreeNode q){
-        if(p==null&&q!=null)return false;
-        if(p!=null&&q==null)return false;
-        if(p==null&&q!=null)return true;
-        if(p.val!=q.val)return false;
+        if(p==null&&q==null)return true;
+        if(p==null||q==null||p.val!=q.val)return false;
         return isSameTree(p.left,q.left)&&isSameTree(p.right,q.right);
     }
 }
