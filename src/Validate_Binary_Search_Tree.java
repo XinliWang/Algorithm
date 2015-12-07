d26a5569087c5cfae4226704076bbd8094a29193
Xinli Wang <wxlmaple@hotmail.com>
2015-12-07 17:32:37 -0500

    Validate Binary Search Tree

--- src/Validate_Binary_Search_Tree.java
+++ src/Validate_Binary_Search_Tree.java
@@ -19,6 +19,9 @@
  * }
  */
 public class Validate_Binary_Search_Tree {
+    /**
+     * Solution1: use long type in case of the value of node is Integer.MIN_VALUE or Integer.MAX_VALUE
+     */
     public boolean isValidBST(TreeNode root) {
         //in case of the value of node is the Min or max value
         return isValidValue(root,(long)Integer.MIN_VALUE-1,(long)Integer.MAX_VALUE+1);
@@ -28,6 +31,22 @@ public class Validate_Binary_Search_Tree {
         //reach the last one successfully
         if(node==null)return true;
         if(node.val<=min || node.val>=max)return false;
+        return isValidValue(node.left,min,node.val) && isValidValue(node.right,node.val,max);
+    }
+
+    /**
+     * Solution2: use object to store min and max value
+     */
+    public boolean isValidBST2(TreeNode root) {
+        //in case of the value of node is the Min or max value
+        return isValidValue2(root,null,null);
+    }
+
+    public boolean isValidValue2(TreeNode node,Integer min,Integer max){
+        //reach the last one successfully
+        if(node==null)return true;
+        if(min!=null && node.val<=min)return false;
+        if(max!=null && node.val<=min)return false;
         return isValidValue(node.left,min,node.val) && isValidValue(node.right,node.val,max);
     }
 }
