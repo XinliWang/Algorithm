90f4e2a0768980259a3c2ef166c5bf05f1639599
Xinli Wang <wxlmaple@hotmail.com>
2015-12-07 18:22:39 -0500

    Valid Parentheses

--- src/Valid_Parentheses.java
+++ src/Valid_Parentheses.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.Stack;
 
 /**
@@ -7,33 +8,51 @@ import java.util.Stack;
  "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
  */
 public class Valid_Parentheses {
+    //Solutio1:
     public boolean isValid(String s){
         Stack<Character> stack = new Stack<>();
-        char temp = '\0';
+
         for(int i=0;i<s.length();i++){
-            temp = s.charAt(i);
+            char temp = s.charAt(i);
             if(temp=='('||temp=='['||temp=='{'){
                 stack.push(temp);
             }else{
-                if(stack.empty()){
-                    return false;
-                }else if(stack.peek()=='('){
-                    if(temp==')')stack.pop();
-                    else return false;
-                }else if(stack.peek()=='['){
-                    if(temp==']')stack.pop();
-                    else return false;
-                }else if(stack.peek()=='{'){
-                    if(temp=='}')stack.pop();
-                    else return false;
+                if(stack.isEmpty())return false;
+                switch (stack.pop()){
+                    case '(': if(temp!=')')return false;
+                              break;
+                    case '[': if(temp!=']')return false;
+                              break;
+                    case '{': if(temp!='}')return false;
+                              break;
                 }
             }
         }
-        if(stack.empty()){
-            return true;
-        }else{
+        if(!stack.empty()){
             return false;
         }
+        return false;
+    }
+    //Solution2: use stack and hashmap
+    public boolean isValid2(String s) {
+        int size = s.length();
+        if(size==0)return true;
+        Stack<Character> stack = new Stack<>();
+        HashMap<Character,Character> map = new HashMap<>();
+        map.put(')','(');
+        map.put(']','[');
+        map.put('}','{');
+        for(int i=0;i<size;i++){
+             char c = s.charAt(i);
+             if(c=='('||c=='['||c=='{'){
+                 stack.push(c);
+             }
+             if(c==')'||c=='}'||c==']'){
+                 if(stack.isEmpty()||map.get(c)!=stack.pop())return false;
+             }
+        }
+        if(!stack.isEmpty())return false;
+        return true;
     }
 
 }
