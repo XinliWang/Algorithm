e17000838fdac2f1e03b66fc3aa39aeda0e11bae
Xinli Wang <wxlmaple@hotmail.com>
2016-01-25 18:26:10 -0500

    SingleNumberIII

--- src/Single_Number_III.java
+++ src/Single_Number_III.java
@@ -2,7 +2,19 @@ import java.util.HashSet;
 import java.util.Iterator;
 
 /**
- * Created by wangxinli on 9/29/15.
+ * Given an array of numbers nums,
+ * in which exactly two elements appear only once and all the other elements appear exactly twice.
+ * Find the two elements that appear only once.
+
+ For example:
+
+ Given nums = [1, 2, 1, 3, 2, 5], return [3, 5].
+
+ Note:
+ The order of the result is not important. So in the above example, [5, 3] is also correct.
+ Your algorithm should run in linear runtime complexity.
+ Could you implement it using only constant space complexity?
+
  */
 public class Single_Number_III {
     //Solution1:bit Manipulation
@@ -27,6 +39,8 @@ public class Single_Number_III {
         }
         return result;
     }
+
+
     public static void main(String[] args){
         Single_Number_III s = new Single_Number_III();
         int[] nums = {1,2,4,1,2,5};
